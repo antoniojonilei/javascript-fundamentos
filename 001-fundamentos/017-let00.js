062957239e89fb9d6e28c8712cf67895c138a893
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-07-30 13:00:02 -0300

    let, let-loop, var-loop

---
+++ 001-fundamentos/017-let00.js
@@ -0,0 +1,10 @@
+let numero = 1
+
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+
+console.log('fora =', numero)
+
+//variaveis definidas com a palavra reservada let tem escopo global, escopo de função e escopo de bloco
