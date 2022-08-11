4c386fb7852ca427916a15d2129aa7c266fd4574
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-11 14:00:13 -0300

    while, doWhile

--- teste.js
+++ teste.js
@@ -21,3 +21,16 @@ function zeldeiro(zelda1, zelda2, aLinkToThePast) {
 console.log(zeldeiro(true, false, false))
 console.log(zeldeiro(true, true, true))
 console.log(zeldeiro(false, false, true))
+
+
+function getInteriroAleatorio(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let numero = 0
+
+while(numero != 4) {
+    numero = getInteriroAleatorio(4, 15)
+    console.log(`Numero selecionado: ${numero}`)
+}
