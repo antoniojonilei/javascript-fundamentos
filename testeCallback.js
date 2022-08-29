4711f1eef441412960917154665f92af3b9c6cf2
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-29 14:21:08 -0300

    Update testeCallback.js

--- testeCallback.js
+++ testeCallback.js
@@ -1,2 +1,13 @@
-const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+let a = 5 
+let b = 10
+let c = 6
 
+if(a > b) {
+    while(b < a ){
+        b = b + 1
+    }
+}else {
+    b = b + c
+}
+
+console.log(b)
