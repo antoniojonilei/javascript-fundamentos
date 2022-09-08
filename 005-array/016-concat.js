f8180e87d9be55949c5062d88e16cb39a24da5dc
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-09-08 16:22:25 -0300

    concat

---
+++ 005-array/016-concat.js
@@ -0,0 +1,6 @@
+const manos = ['Bill', 'Brandon']
+const minas = ['ashley', 'trinity']
+const todos = manos.concat(minas, 'fulano')
+console.log(todos)
+
+console.log([].concat([1, 2], [2, 3], 5, [[6, 7]]))
